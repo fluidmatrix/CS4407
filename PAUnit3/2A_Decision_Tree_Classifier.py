952127aacaf331d3be3ea5d9dbac1ba9a199cf45
fluidmatrix <loner.mischief@gmail.com>
2026-07-05 16:59:29 +0530

    updated Jupyter Notebook

--- PAUnit3/2A_Decision_Tree_Classifier.py
+++ PAUnit3/2A_Decision_Tree_Classifier.py
@@ -1,17 +1,17 @@
-# Importing Necessary Libraries and Modules
+# # Importing Necessary Libraries and Modules
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from utils import print_heatmap
 
-# Loading the Iris Dataset
+# # Loading the Iris Dataset
 iris = load_iris()
 
-# Assigning the Variables
+# # Assigning the Variables
 X = iris.data 
 y = iris.target
 
-# Splitting the dataset into Train and Test
+# # Splitting the dataset into Train and Test
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -20,14 +20,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-# Loading the Decision Tree Classifier
+# # Loading the Decision Tree Classifier
 DecisionTree = DecisionTreeClassifier(random_state=42)
 
-# Fitting the Model on our Dataset
+# # Fitting the Model on our Dataset
 DecisionTree.fit(X_train, y_train)
 
-# Making the Prediction
+# # Making the Prediction
 DecisionTree_Prediction = DecisionTree.predict(X_test)
 
-# Printing the Metrics
+# # Printing the Metrics
 print_heatmap( y_test=y_test, y_predict=DecisionTree_Prediction)
